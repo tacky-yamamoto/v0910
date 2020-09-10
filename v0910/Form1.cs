757a6cda3e74cecbae5267139d9baa8deed5a063
A601-22 <tnk.dat.class@gmail.com>
2020-09-10 09:38:44 +0900

    復讐-1

--- v0910/Form1.cs
+++ v0910/Form1.cs
@@ -12,10 +12,25 @@ namespace v0910
 {
     public partial class Form1 : Form
     {
+        private static Random rand = new Random();
+        int[] vx = new int[10];
+        int[] vy = new int[10];
+        Label[] labels = new Label[10];
 
         public Form1()
         {
             InitializeComponent();
+            for(int i=0;i<10;i++)
+            {
+                vx[i] = rand.Next(-15,16);
+                vy[i] = rand.Next(-15, 16);
+                labels[i] = new Label();
+                labels[i].AutoSize = true;
+                labels[i].Text = "(´・ω・`)";
+                Controls.Add(labels[i]);
+                labels[i].Left = rand.Next(0, ClientSize.Width - labels[i].Width);
+                labels[i].Top = rand.Next(0, ClientSize.Height - labels[i].Height);
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
